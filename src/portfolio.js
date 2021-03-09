76d1ef9e6011a6d06713c8612ba2fff275f80d44
krokhale <krishna.rokhale@gmail.com>
2021-03-08 17:53:47 -0800

    checking in

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,9 +1,66 @@
 import React, {useState, useEffect} from 'react';
 
 function Portfolio(props) {
+
+    const [currentWallet, setCurrentWallet] = useState();
+    const [currentPortfolio, setCurrentPortfolio] = useState();
+
+    const fetchWallet = async () => {
+        const res = await fetch(`http://localhost:3000/api/v1/wallet`);
+        let json = await res.json();
+        console.log(json)
+        setCurrentWallet(json)
+    };
+
+    const fetchPortfolio = async () => {
+        const res = await fetch(`http://localhost:3000/api/v1/portfolio`);
+        let json = await res.json();
+        console.log('this is the portfolio json', json)
+        setCurrentPortfolio(json);
+
+    };
+
+    useEffect(() => {
+        console.log('this runs only once on component load')
+        fetchWallet()
+        fetchPortfolio()
+    }, [])
+
+
+
     return (
         <div className={'border p-5'}>
-            BOX TWO
+
+            <h1 className={'text-xl font-bold'}>Portfolio</h1>
+            {currentPortfolio && <table style={{width: '100%'}}>
+                <thead>
+                    <th className={'border'}>Stock</th>
+                    <th className={'border'}>Quantity</th>
+                    <th className={'border'}>Value</th>
+                </thead>
+                <tbody>
+                    {currentPortfolio.map((item, idx) => {
+                        return <tr key={idx}>
+                            <td className={'border text-center'}>{item.symbol}</td>
+                            <td className={'border text-center'}>{item.quantity}</td>
+                            <td className={'border text-center'}>{item.price}</td>
+                        </tr>
+
+                    })}
+
+
+                </tbody>
+            </table>}
+
+            {!currentPortfolio && <h1>Loading...</h1>}
+
+            <br/>
+            <br/>
+            <br/>
+
+            {currentWallet && <h1 className={'text-xl font-bold'}>Current Wallet Value: ${currentWallet.value}</h1>}
+
+
         </div>
     );
 }
--- src/search.js
+++ src/search.js
@@ -11,18 +11,107 @@ function Search(props) {
     //     console.log(quotes)
     // });
     const [inputText, setInputText] = useState('');
+    const [currentStock, setCurrentStock] = useState();
+    const [ticker, setTicker] = useState();
+
+    const [buyQuantity, setBuyQuantity] = useState(0);
+
+    const [currentWallet, setCurrentWallet] = useState();
+
+    const fetchWallet = async () => {
+        const res = await fetch(`http://localhost:3000/api/v1/wallet`);
+        let json = await res.json();
+        console.log(json)
+        setCurrentWallet(json)
+    };
+
+    useEffect(() => {
+        console.log('this runs only once on component load')
+
+
+        fetchWallet()
+    }, [])
 
 
     const fetchQuote = async () => {
-        const res = await fetch(`http://localhost:3000/api/v1/stocks/search/${inputText}`);
+        const res = await fetch(`http://localhost:3000/api/v1/portfolio/search/${inputText}`);
         let json = await res.json();
         console.log(json)
+        setCurrentStock(json);
+        setTicker(inputText);
         setInputText('')
     };
 
     const onInputChange = async (ev) => {
         console.log(ev.currentTarget.value)
         setInputText(ev.currentTarget.value);
+    };
+
+    // user clicks on the buy button
+    // on button click you trigger a function
+    // make a POST request to the API and update the portfolio
+
+
+    const buyStock = async () => {
+        console.log('buying the stock now')
+        console.log(currentWallet)
+
+        // check if the quantity is not 0
+        // check if the user has enough cash in teh wallet to be able to make a purchase
+        // if not show an error
+        // if yes, make a purchase and add a field in the portfolio table
+
+        if(buyQuantity == 0){
+            alert('Buy quantity needs to be greater than 0!')
+        }
+
+        let cashNeeded = buyQuantity * currentStock.data.price;
+        console.log('cashNeeded is', cashNeeded)
+
+        if(cashNeeded > currentWallet.value){
+            alert('You dont have enough cash!');
+        } else {
+            // make the purchase
+            // making a field in the database table for portfolio
+            let body = {
+                symbol: ticker,
+                quantity: buyQuantity,
+                price: currentStock.data.price,
+
+            }
+
+            let options = {
+                method: 'POST',
+                body: JSON.stringify(body),
+                headers: {}
+
+            };
+
+            options.headers["Accept"] = "application/json, text/plain, */*";
+            options.headers["Content-Type"] = "application/json;charset=utf-8";
+
+            console.log(options);
+
+
+            const res = await fetch(`http://localhost:3000/api/v1/portfolio`, options);
+            let json = await res.json();
+            console.log(json)
+
+            setBuyQuantity(0)
+
+            alert('Success!')
+
+
+
+
+        }
+
+        // stock symbol
+        // stock quote
+    };
+
+    const onBuyChange = async (ev) => {
+        setBuyQuantity(ev.currentTarget.value);
     };
 
     return (
@@ -37,17 +126,21 @@ function Search(props) {
                 </div>
             </div>
 
-            <div className="grid grid-cols-2">
+            {ticker && <div className="grid grid-cols-2">
                 <div className={'border p-5'}>
-                    <h1 className={'text-2xl'}>BHP - BHP Group Limited</h1>
+                    {/*{currentStock && <h1 className={'text-2xl'}>{currentStock.data}</h1>}*/}
+                    <h1 className={'text-2xl'}>{ticker} : {currentStock && <span>&nbsp;&nbsp;{currentStock.data.currency} {currentStock.data.price}</span>}</h1>
                 </div>
                 <div className={'border p-5'}>
 
-                    <span className={'bg-blue-600 cursor-pointer p-2 rounded text-white text-xl pl-5 pr-5'}>Buy</span>&nbsp;&nbsp;&nbsp;
-                    <span className={'bg-red-600 cursor-pointer p-2 rounded text-white text-xl pl-5 pr-5'}>Sell</span>
+                    <span>
+                        <input type="number" onChange={onBuyChange} className={'border'} value={buyQuantity} />&nbsp;&nbsp;
+                        <span className={'bg-blue-600 cursor-pointer p-2 rounded text-white text-xl pl-5 pr-5'} onClick={buyStock}>Buy</span>&nbsp;&nbsp;&nbsp;
+                        </span>
+                    {/*<span className={'bg-red-600 cursor-pointer p-2 rounded text-white text-xl pl-5 pr-5'}>Sell</span>*/}
 
                 </div>
-            </div>
+            </div>}
 
         </div>
     );
